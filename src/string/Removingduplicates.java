79ab1f36d75b12be1c088ac514071e2d3f1d35c3
vamsi <vamcc3@gmail.com>
2017-08-10 00:46:57 -0400

    hi sai

--- src/string/Removingduplicates.java
+++ src/string/Removingduplicates.java
@@ -4,7 +4,7 @@ public class Removingduplicates {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] input = {1, 7,3,3,3,3,10,23};
+		int[] input = {1, 7,3,3,3,10,23};
 		int current = input[0];
 		//boolean found = false;
 		//System.out.println(input.length);
